d39ea7fe8b5e99a48149bb904bdbecb74999dfce
marekhacieja <marek.hacieja@gmail.com>
2021-03-22 17:22:34 +0100

    storing theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector : 'app-root',
   templateUrl : './app.component.html',
   styleUrls : [ './app.component.scss' ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'planning-poker';
 
-  background(): void {
+  ngOnInit(): void {
+    const backgroundStyle = localStorage.getItem('background-style');
+
+    if (backgroundStyle.match('white')) {
+      document.getElementById('gradient').style.background = 'white';
+    } else {
+      document.getElementById('gradient').style.background = '';
+    }
+  }
+
+  changeBackground(): void {
     const style = document.getElementById('gradient').style.background;
     if (style.match('white')) {
+      localStorage.setItem('background-style', 'gradient');
       document.getElementById('gradient').style.background = '';
     } else {
+      localStorage.setItem('background-style', 'white');
       document.getElementById('gradient').style.background = 'white';
     }
   }
