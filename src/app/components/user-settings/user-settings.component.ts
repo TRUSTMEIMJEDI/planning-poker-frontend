b83498bda319f31ad15b8887f6cf1bef33f990c6
marekhacieja <marek.hacieja@gmail.com>
2021-07-18 15:51:37 +0200

    bug fix; change room type

--- src/app/components/user-settings/user-settings.component.ts
+++ src/app/components/user-settings/user-settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { UserDataService } from '../../services/user-data.service';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
@@ -7,13 +7,15 @@ import { MatMenuTrigger } from '@angular/material/menu';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { RoomType } from '../../models/room-type';
 import { PokerService } from '../../services/poker.service';
+import { Message } from '@stomp/stompjs';
+import { RxStompService } from '@stomp/ng2-stompjs';
 
 @Component({
   selector : 'app-user-settings',
   templateUrl : './user-settings.component.html',
   styleUrls : [ './user-settings.component.scss' ]
 })
-export class UserSettingsComponent implements OnInit {
+export class UserSettingsComponent implements OnInit, OnDestroy {
 
   @Input() allowDeleteUsers: boolean;
   @Output() allowDeleteUsersEvent = new EventEmitter<boolean>();
@@ -32,18 +34,26 @@ export class UserSettingsComponent implements OnInit {
     { roomType : RoomType.MOSCOW, desc : 'MOSCOW' }
   ];
 
+  private changeRoomTypeSub$: Subscription;
   private userSub$: Subscription;
 
   constructor(public userDataService: UserDataService,
               private userService: UserService,
               private router: Router,
               private snackBar: MatSnackBar,
-              private pokerService: PokerService) { }
+              private pokerService: PokerService,
+              private rxStompService: RxStompService) { }
 
   ngOnInit(): void {
     this.userSub$ = this.userDataService.currentUser.subscribe(() => {
       this.init();
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.changeRoomTypeSub$) {
+      this.changeRoomTypeSub$.unsubscribe();
+    }
   }
 
   changeUserType(): void {
@@ -115,7 +125,7 @@ export class UserSettingsComponent implements OnInit {
   }
 
   changeRoomType(roomType: string): void {
-    this.pokerService.changeRoomType(RoomType[roomType], this.userDataService.currentUserValue.roomKey)
+    this.pokerService.changeRoomType(RoomType[ roomType ], this.userDataService.currentUserValue.roomKey)
       .subscribe();
   }
 
@@ -124,6 +134,15 @@ export class UserSettingsComponent implements OnInit {
     this.observer = this.userDataService.currentUserValue?.observer;
     this.currentUsername = this.userDataService.currentUserValue?.userName;
     this.newUsername = this.currentUsername;
+
+    if (this.isInRoom) {
+      this.changeRoomTypeSub$ = this.rxStompService.watch('/room/' + this.userDataService.currentUserValue?.roomKey + '/changeType')
+        .subscribe((message: Message) => {
+          if (message.body) {
+            this.userDataService.currentUserValue.roomType = message.body;
+          }
+        });
+    }
   }
 
   private changePassword(isInRoom: boolean): void {
